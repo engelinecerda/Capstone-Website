540a723830f8dd57d9d37ebf2d0b22010e883044
Bryan <bryan.singson.7@gmail.com>
2026-08-25 16:28:38 +0800

    edited inactivity timeout

--- js/super_admin_inactivity.js
+++ js/super_admin_inactivity.js
@@ -5,14 +5,15 @@ export function setupInactivityLogout(role) {
     if (role !== "admin") return;
 
     let timeout;
-    const INACTIVITY_LIMIT = 2 * 60 * 60 * 1000; // 2 hours
+    
+    const INACTIVITY_LIMIT = 2 * 60 * 60 * 1000;     
 
     function resetTimer() {
         clearTimeout(timeout);
         timeout = setTimeout(async () => {
             alert("Logged out due to inactivity.");
             await supabase.auth.signOut();
-            window.location.href = "./admin_login.html";
+            window.location.href = "./index.html";
         }, INACTIVITY_LIMIT);
     }
 
